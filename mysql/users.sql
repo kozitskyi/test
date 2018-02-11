ab06ca96c80f124d7e085232697152f2a2ef0460
Vadim <extremel@bk.ru>
2018-02-11 11:44:40 +0200

    refresh

--- mysql/users.sql
+++ mysql/users.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.3
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Фев 11 2018 г., 01:22
--- Версия сервера: 5.7.16
--- Версия PHP: 7.1.0
+-- Хост: localhost:3306
+-- Время создания: Фев 11 2018 г., 11:40
+-- Версия сервера: 5.6.36-cll-lve
+-- Версия PHP: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `trainee`
+-- База данных: `dtnqaexg_users`
 --
 
 -- --------------------------------------------------------
@@ -31,8 +33,8 @@ CREATE TABLE `users` (
   `name` varchar(20) NOT NULL,
   `login` varchar(20) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `activation` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `activation` varchar(60) NOT NULL,
   `status` enum('1','2') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -56,7 +58,8 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
